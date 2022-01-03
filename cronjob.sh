63dfe9693cc737443d1f9618df0c6db1d9184247
Manoel Brunnen <mb@lee-brunnen.de>
2022-01-03 01:10:58 +0100

    Install requirements

--- cronjob.sh
+++ cronjob.sh
@@ -4,5 +4,7 @@ set -eu
 
 cd $(dirname "$0") && pwd
 
+python3 -m venv env
 . env/bin/activate
+pip3 install -r requirements.txt
 REQUESTS_CA_BUNDLE=/etc/ssl/certs/ca-certificates.crt ./kimsufi.py --mail KS-2
